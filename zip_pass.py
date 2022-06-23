24c8dec31230f753f569c54791d53ca4da0b6a62
ErmiasBahru <ermiasbahru878@gmail.com>
2022-06-23 11:02:49 +0300

    added argument feature

--- zip_pass.py
+++ zip_pass.py
@@ -14,7 +14,7 @@ n_words = len(list(open(wordlist, "rb")))
 # print the total number of passwords
 print("Total passwords to test:", n_words)
 with open(wordlist, "rb") as wordlist:
-    for word in tqdm(wordlist, total=n_words, unit="word"):
+    for word in tqdm(wordlist, total=n_words, unit=" word"):
         try:
             zip_file.extractall(pwd=word.strip())
         except:
