9ba281ec9a7b67c64ae068a9201370ef9f9dc819
ErmiasBahru <ermiasbahru878@gmail.com>
2022-06-23 10:52:32 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
-# How to Brute Force ZIP File Passwords in Python
+# Brute Force ZIP File Passwords in Python
 
-*created by Ermias Bahru*
+[live demo](https://vimeo.com/723258767)
+
+### Required:
+
+1. zipfile (built-in module)
+2. tqdm
+
+### Install the required dependencies:
+
+`$ pip install tqdm`
+
+### Usage:
+`$ python zip_pass.py <ZipFileYouWantToCrack> <Wordlist>`
+
+### Example:
+`$ python zip_pass.py my_secret.zip wordlist.txt`
+
+### Screenshot:
+
+<img src="demo.png">
+
+### WARNING:
+
+User for learning purpose only!
--- zip_pass.py
+++ zip_pass.py
@@ -1,22 +1,25 @@
 from tqdm import tqdm
-import zipfile
 
-wordlist = 'wordlist.txt'
+import zipfile
+import sys
+
+# the password list path
+wordlist = sys.argv[2]
 # the zip file you want to crack its password
-zip_file = 'example.zip'
+zip_file = sys.argv[1]
 
 zip_file = zipfile.ZipFile(zip_file)
-num_of_wordlist = len(list(open(wordlist, 'rb')))
-
-# print('total password to test', num_of_wordlist)
-
-with open(wordlist, 'rb') as wordlist:
-    for word in tqdm(wordlist, total=num_of_wordlist, unit='word'):
+# count the number of words in this wordlist
+n_words = len(list(open(wordlist, "rb")))
+# print the total number of passwords
+print("Total passwords to test:", n_words)
+with open(wordlist, "rb") as wordlist:
+    for word in tqdm(wordlist, total=n_words, unit="word"):
         try:
             zip_file.extractall(pwd=word.strip())
         except:
             continue
         else:
-            print('[+] Password found:', word.decode().strip())
+            print("[+] Password found:", word.decode().strip())
             exit(0)
-print('[!] Password not found, try other wordlist.')
+print("[!] Password not found, try other wordlist.")
