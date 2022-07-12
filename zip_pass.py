485948642dd653841a6f4103b56ada3f9c074e02
MattTheCoder-W <mateusz.wasa@protonmail.com>
2022-07-12 14:39:58 +0200

    Added argparse

--- zip_pass.py
+++ zip_pass.py
@@ -1,12 +1,28 @@
 from tqdm import tqdm
 
 import zipfile
-import sys
+
+import argparse
+from os import path
+
+
+def file_location(location: str) -> str:
+    if path.exists(location):
+        return location
+    else:
+        print(f"Error: Specified file does not exist -> {location}")
+        exit(1)
+
+
+parser = argparse.ArgumentParser(description="Brute force ZIP file passwords")
+parser.add_argument('zip_file', type=file_location, help="Password protected zip file")
+parser.add_argument('wordlist_file', type=file_location, help="Wordlist file")
+args = vars(parser.parse_args())
 
 # the password list path
-wordlist = sys.argv[2]
+wordlist = args['wordlist_file']
 # the zip file you want to crack its password
-zip_file = sys.argv[1]
+zip_file = args['zip_file']
 
 zip_file = zipfile.ZipFile(zip_file)
 # count the number of words in this wordlist
